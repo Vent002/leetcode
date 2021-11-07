ce930045b81f37e6d8baad65a6138bac7414c039
gxming <gxminglee@qq.com>
2021-11-07 15:43:38 +0800

    day25

---
+++ learn-leetcode/src/main/java/cn/hsmxg1204/learnday25/IsValidBST.java
@@ -0,0 +1,65 @@
+package cn.hsmxg1204.learnday25;
+
+/**
+ * TODO
+ *
+ * @author gxming
+ * @description 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ *
+ * 有效 二叉搜索树定义如下：
+ *
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [2,1,3]
+ * 输出：true
+ * 示例 2：
+ *
+ *
+ * 输入：root = [5,1,4,null,null,3,6]
+ * 输出：false
+ * 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点数目范围在[1, 104] 内
+ * -231 <= Node.val <= 231 - 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @date 2021-11-07 15:38
+ */
+public class IsValidBST {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    // (min,max)
+    public boolean isValidBST(TreeNode p,long min,long max)
+    {
+        if(p == null)
+            return true;
+        if(p.val <= min || p.val >= max)
+            return false;
+        return isValidBST(p.left,min,p.val) && isValidBST(p.right,p.val,max);
+    }
+}
---
+++ learn-leetcode/src/main/java/cn/hsmxg1204/learnday25/MaxDepth.java
@@ -0,0 +1,49 @@
+package cn.hsmxg1204.learnday25;
+
+/**
+ * TODO
+ *
+ * @author gxming
+ * @description 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ *
+ * @date 2021-11-07 15:34
+ */
+public class MaxDepth {
+
+      //Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    public static int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + (left >= right?left:right);
+    }
+}
