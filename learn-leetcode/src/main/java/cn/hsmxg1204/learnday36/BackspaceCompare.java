2a21caf8f7668cd0b6da1a3dd5298dad8bf20cbb
gxming <gxminglee@qq.com>
2021-11-24 10:13:11 +0800

    'modify'

--- learn-leetcode/src/main/java/cn/hsmxg1204/learnday36/BackspaceCompare.java
+++ learn-leetcode/src/main/java/cn/hsmxg1204/learnday36/BackspaceCompare.java
@@ -1,8 +1,34 @@
-package cn.hsmxg1204.learnday36;/**
-* TODO 
-*
-* @description 
-* @author gxming
-* @date 2021-11-24 9:17
-*/public class BackspaceCompare {
+package cn.hsmxg1204.learnday36;
+
+/**
+ * TODO
+ *
+ * @author gxming
+ * @description 844. 比较含退格的字符串
+ * @date 2021-11-24 9:17
+ */
+public class BackspaceCompare {
+    public static boolean backspaceCompare(String s,String t){
+        return build(s).equals(build(t));
+    }
+    public static String build(String s){
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char chs = s.charAt(i);
+            if(chs != '#'){
+                newStr.append((char) chs);
+            }else{
+                if (newStr.length() > 0){
+                    newStr.deleteCharAt(newStr.length() - 1);
+                }
+            }
+        }
+        return newStr.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "ab#c";
+        String t = "ad#c";
+        System.out.println(backspaceCompare(s, t));
+    }
 }
--- learn-leetcode/src/main/java/cn/hsmxg1204/learnday36/OriginalDigits.java
+++ learn-leetcode/src/main/java/cn/hsmxg1204/learnday36/OriginalDigits.java
@@ -1,5 +1,8 @@
 package cn.hsmxg1204.learnday36;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * TODO
  *
@@ -8,4 +11,39 @@ package cn.hsmxg1204.learnday36;
  * @date 2021-11-24 8:45
  */
 public class OriginalDigits {
+
+    public static String originalDigits(String s){
+        Map<Character,Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        int[] cnt = new int[10];
+
+        cnt[0] = map.getOrDefault('z',0);
+        cnt[2] = map.getOrDefault('w',0);
+        cnt[4] = map.getOrDefault('u',0);
+        cnt[6] = map.getOrDefault('x',0);
+        cnt[8] = map.getOrDefault('g',0);
+
+        cnt[3] = map.getOrDefault('h',0) - cnt[8];
+        cnt[5] = map.getOrDefault('f',0) - cnt[4];
+        cnt[7] = map.getOrDefault('s',0) - cnt[6];
+
+        cnt[1] = map.getOrDefault('o',0) - cnt[0] - cnt[2] - cnt[4];
+        cnt[9] = map.getOrDefault('i',0) - cnt[5] - cnt[6] - cnt[8];
+
+        StringBuffer ans = new StringBuffer();
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < cnt[i]; ++j) {
+                ans.append((char) (i + '0'));
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "owoztneoer";
+        System.out.println(originalDigits(s));
+    }
 }
