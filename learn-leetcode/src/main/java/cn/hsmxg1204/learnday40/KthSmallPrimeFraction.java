feda78105dcb4f822936a13bc08740a455fa94bc
gxming <gxminglee@qq.com>
2021-11-29 09:09:49 +0800

    '添加注释'

--- learn-leetcode/src/main/java/cn/hsmxg1204/learnday40/KthSmallPrimeFraction.java
+++ learn-leetcode/src/main/java/cn/hsmxg1204/learnday40/KthSmallPrimeFraction.java
@@ -9,7 +9,26 @@ import java.util.List;
  * TODO
  *
  * @author gxming
- * @description
+ * @description 786. 第 K 个最小的素数分数
+ * 给你一个按递增顺序排序的数组 arr 和一个整数 k 。数组 arr 由 1 和若干 素数  组成，且其中所有整数互不相同。
+ *
+ * 对于每对满足 0 < i < j < arr.length 的 i 和 j ，可以得到分数 arr[i] / arr[j] 。
+ *
+ * 那么第 k 个最小的分数是多少呢?  以长度为 2 的整数数组返回你的答案, 这里 answer[0] == arr[i] 且 answer[1] == arr[j] 。
+ *
+ *  
+ * 示例 1：
+ *
+ * 输入：arr = [1,2,3,5], k = 3
+ * 输出：[2,5]
+ * 解释：已构造好的分数,排序后如下所示:
+ * 1/5, 1/3, 2/5, 1/2, 3/5, 2/3
+ * 很明显第三个最小的分数是 2/5
+ * 示例 2：
+ *
+ * 输入：arr = [1,7], k = 1
+ * 输出：[1,7]
+ *
  * @date 2021-11-29 8:49
  */
 public class KthSmallPrimeFraction {
