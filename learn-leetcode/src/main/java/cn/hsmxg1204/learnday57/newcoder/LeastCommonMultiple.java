25dd8d86311cb87a63217ffdc6cc8ed1d4a58ca5
gxming <gxminglee@qq.com>
2022-03-03 09:15:57 +0800

    revise

--- learn-leetcode/src/main/java/cn/hsmxg1204/learnday57/newcoder/LeastCommonMultiple.java
+++ learn-leetcode/src/main/java/cn/hsmxg1204/learnday57/newcoder/LeastCommonMultiple.java
@@ -1,7 +1,31 @@
-package cn.hsmxg1204.learnday57.newcoder;/**
+package cn.hsmxg1204.learnday57.newcoder;
+
+import java.util.Scanner;
+
+/**
  * @Author gxming
- *
- * @Description 
+ * @Description 最小公倍数
+ * a，b的最大公约数为gcd（a，b），最小公倍数的公式为：
+ * lcm(a,b) = ｜a * b｜ / gcd(a,b)
  * @CreateTime 2022-03-01 20:22
- */public class LeastCommonMultiple {
+ */
+public class LeastCommonMultiple {
+    /**
+     * 欧几里得求最大公约数，根据公式ab=最小公倍数gcd(a,b)，得到最小公倍数。
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int leastCommonMultiple(int a, int b){
+        return b == 0 ? a : leastCommonMultiple(b,a % b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(5 * 7 / leastCommonMultiple(5, 7));
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println( a * b / leastCommonMultiple(a,b));
+    }
 }
--- learn-leetcode/src/main/java/cn/hsmxg1204/learnday57/newcoder/TransformTri.java
+++ learn-leetcode/src/main/java/cn/hsmxg1204/learnday57/newcoder/TransformTri.java
@@ -1,7 +1,40 @@
-package cn.hsmxg1204.learnday57.newcoder;/**
+package cn.hsmxg1204.learnday57.newcoder;
+
+import java.util.Scanner;
+
+/**
  * @Author gxming
+ * @Description 变形的杨辉三角
+ * 第一行只有一个数1，以下每行的每个数，是恰好是它上面的数、左上角数和右上角的数，3个数之和（如果不存在某个数，认为该数就是0）。
+ * 求第n行第一个偶数出现的位置。如果没有偶数，则输出-1。例如输入3,则输出2，输入4则输出3。
+ * 数据范围：
+ * 1 ≤ n ≤10^9
  *
- * @Description 
+ * 1≤n≤10^9
+ *
+ * 本题有多组输入数据
+ * 输入描述：
+ *
+ * 输入一个int整数
+ * 输出描述：
+ *
+ * 输出返回的int值
  * @CreateTime 2022-03-01 20:52
- */public class TransformTri {
+ */
+public class TransformTri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            int num = sc.nextInt();
+            if(num == 1 || num == 2){
+                System.out.println(-1);
+            }else if(num % 4 == 0){
+                System.out.println(2);
+            }else if(num % 4 == 2){
+                System.out.println(4);
+            }else{
+                System.out.println(3);
+            }
+        }
+    }
 }
