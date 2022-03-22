0ca17cf64f166e183bc40dd920b3be9bfe7e38c0
gxming <gxminglee@qq.com>
2022-03-22 19:11:17 +0800

    统计按位或能得到最大值的子集数目 未完成

---
+++ learn-leetcode/src/main/java/cn/hsmxg1204/learnday61/CountMaxOrSubsets.java
@@ -0,0 +1,17 @@
+package cn.hsmxg1204.learnday61;
+
+/**
+ * @Author gxming
+ * @Description 2044. 统计按位或能得到最大值的子集数目
+ * @CreateTime 2022-03-15 12:01
+ */
+public class CountMaxOrSubsets {
+    public static int countMaxOrSubsets(int[] nums){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
